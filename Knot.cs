60683b3b237dc1e7221c9bfaae654d23c56bbf05
SadikBAL <sadikbal@hotmail.com>
2024-03-01 11:36:30 +0300

    First Commit

---
+++ Knot.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CresciveGamesAlgorithm
+{
+    public  class Knot
+    {
+        private string KnotName;
+        List<Knot> ChildKnots;
+        public Knot(string Name)
+        {
+            KnotName = Name;
+            ChildKnots = new List<Knot>();
+        }
+        public void AddKnot(Knot child)
+        {
+            ChildKnots.Add(child);
+        }
+        public void PrintGroup()
+        {
+            Console.WriteLine("---> Result for KnotName : " + KnotName + " <---");
+            List<Knot> Test = new List<Knot>();
+            GetUniqeChild(ref Test);
+            foreach (Knot child in Test) 
+            {
+                Console.Write(child.KnotName.ToString() + " ");
+            }
+            Console.WriteLine("\n---------------------------------------");
+        }
+        public void GetUniqeChild(ref List<Knot> output)
+        {
+            if(!output.Contains(this))
+            {
+                output.Add(this);
+                foreach (Knot child in ChildKnots)
+                {
+                    child.GetUniqeChild(ref output);
+                }
+            }
+        }
+        
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CresciveGamesAlgorithm;
+
+Console.WriteLine("Hello, Crescive Games <3 \n");
+
+Knot Yellow = new Knot("Yellow");
+Knot Blue = new Knot("Blue");
+Knot Red = new Knot("Red");
+Knot Pink = new Knot("Pink");
+Knot Purple = new Knot("Purple");
+Knot Orange = new Knot("Orange");
+
+Yellow.AddKnot(Blue);
+Yellow.AddKnot(Red);
+Blue.AddKnot(Yellow);
+Red.AddKnot(Yellow);
+Red.AddKnot(Pink);
+Pink.AddKnot(Red);
+Purple.AddKnot(Orange);
+Orange.AddKnot(Purple);
+
+Yellow.PrintGroup();
+Purple.PrintGroup();
+Blue.PrintGroup();
